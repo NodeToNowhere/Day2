d77efd177cf370a3043ba475310d7e8717e795f9
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro.local>
2021-11-10 01:11:18 +0000

    Totally worth losing sleep over

--- ex_c_uk_EXT.py
+++ ex_c_uk_EXT.py
@@ -17,6 +17,41 @@ united_kingdom = [
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+
+united_kingdom[2]["name"] = "Cardiff"
+print(united_kingdom[1]["name"])
+
 # 2. Create a dictionary for Northern Ireland and add it to the `united_kingdom` list (The capital is Belfast, and the population is 1,811,000).
+
+northern_ireland = {
+    "name": "Northern Ireland",
+    "population": 1811000,
+    "capital": "Belfast"
+  }
+united_kingdom.append(northern_ireland)
+print(united_kingdom)
+
 # 3. Use a loop to print the names of all the countries in the UK.
+print('=========================')
+
+# for i in united_kingdom:
+#   for key in i.keys():
+#     if key == "name":
+#       print(key) 
+      
+for index in range(len(united_kingdom)):
+  for key in united_kingdom[index]:
+    if key == "name":
+      print(united_kingdom[index][key])
+    
+    
 # 4. Use a loop to find the total population of the UK.
+print('=========================')
+
+total_pop = 0
+
+for index in range(len(united_kingdom)):
+  for key in united_kingdom[index]:
+    if key == "population":
+      total_pop += united_kingdom[index][key]
+print(total_pop)
